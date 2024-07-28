78032fe209e4222b36f10c3b5302f6a5d32a79c8
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-07-28 11:30:04 +0300

    ыер

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -169,7 +169,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 }
 //handler.help = ['help']
 //handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
+handler.command = ['кепорукерукртвапрцкркено'] 
 handler.register = false
 
 export default handler
