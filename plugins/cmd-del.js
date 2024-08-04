b08ffe0879a4f777a4df82c9197c2dbaecf3cdad
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-08-04 13:40:52 +0300

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -3,7 +3,7 @@
 let handler = async (m, { text }) => {
     let hash = text
     if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `✳️ Введите имя комамдо`
+    if (!hash) throw `✳️ Введите имя команды`
     let sticker = global.db.data.sticker
     if (sticker[hash] && sticker[hash].locked) throw '✳️ Вы не можете удалить эту команду'
     delete sticker[hash]
