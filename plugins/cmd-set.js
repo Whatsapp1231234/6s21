1c6388e75c213b47d6c59086363a311df16386e6
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2024-08-04 13:46:39 +0300

    впрьт

--- plugins/cmd-set.js
+++ plugins/cmd-set.js
@@ -3,10 +3,10 @@ let handler = async (m, { text, usedPrefix, command }) => {
     global.db.data.sticker = global.db.data.sticker || {}
     if (!m.quoted) throw `✳️ ${mssg.reply}`
     if (!m.quoted.fileSha256) throw `⚠️ ${mssg.reply}`
-    if (!text) throw `✳️ Falta el comando`
+    if (!text) throw `✳️ Команда отсутствует`
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('base64')
-    if (sticker[hash] && sticker[hash].locked) throw '⚠️ No tienes permiso para cambiar este comando de Sticker'
+    if (sticker[hash] && sticker[hash].locked) throw '⚠️ У вас нет разрешения изменять эту команду с Sticker'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
